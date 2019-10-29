bed8ad4c0f2b6acd702f389ea82299c55b76aef9
Kizan Mykola <bestplaceforwork@gmail.com>
2019-10-29 02:07:21 +0200

    edit PDO and validation

--- handler.php
+++ handler.php
@@ -3,25 +3,24 @@ session_start();
 include_once 'db.php';
 require_once 'functions.php';
 
-$name = htmlspecialchars($_POST['name']);
-$text = htmlspecialchars($_POST['text']);
+$name = filter_input(INPUT_POST,'name',FILTER_SANITIZE_STRING);
+$text = filter_input(INPUT_POST,'text',FILTER_SANITIZE_STRING);
 
 if(!empty($name) && !empty($text)){
 
     if(check_length($name, 4, 20) && check_length($text, 2, 1000)){
+
         //SQL запрос в БД на вставку данных
         $insert = "INSERT INTO blog.comments (`name`, `comments`) 
-        VALUES (:name, :text,);";
+        VALUES (?,?);";
 
         //Так как в запросе есть переменная, его нужно сперва подготовить
         $statement = $pdo->prepare($insert);
         
         //Выполняем
-        $statement->execute($_POST);
+        $statement->execute([$name, $text]);
         $_SESSION['flash'] = 'Ваш комментарий добавлен';
-        /*if(clean($name) && clean($text)){
-            
-        }*/
+        
     }
 }
 
